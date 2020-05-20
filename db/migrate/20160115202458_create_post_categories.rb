36a9b47aad86cde537c79dc139490c83945f1a61
Seanbon0611 <seand0611@gmail.com>
2020-05-20 10:08:10 -0700

    Done.

--- db/migrate/20160115202458_create_post_categories.rb
+++ db/migrate/20160115202458_create_post_categories.rb
@@ -3,7 +3,7 @@ class CreatePostCategories < ActiveRecord::Migration
     create_table :post_categories do |t|
       t.belongs_to :post, index: true, foreign_key: true
       t.belongs_to :category, index: true, foreign_key: true
-      t.timestamps null: false
+      t.timestamps null: false 
     end
   end
 end
